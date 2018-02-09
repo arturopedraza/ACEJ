f2e58fa34c03ddcc6092d1d6635fe6b206fa49c0
ARTURO <arturopedraza05@gmail.com>
2018-02-08 22:41:31 -0600

    primera version

---
+++ JS/app.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+     $(".btn").sideNav({
+         menuWidth: 200, 
+     });
+     
+})
+
+  $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+
+
+$(document).ready(function(){
+    $(".button-collapse").sideNav();
+});
+
+  $(document).ready(function(){
+      $('.carousel').carousel();
+    });
